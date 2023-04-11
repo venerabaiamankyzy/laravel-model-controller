31c0e52bd540b5372487b1b863222417a2867737
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-04-11 08:48:06 +0200

    added movie controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,12 +10,11 @@ class MovieController extends Controller
     public function movieList() {
         
         $movies = Movie::all();
-        // dd($movies);
         return view('movie-list', compact('movies'));
     }
+    
     public function movieDetail($id) {        
         $movie = Movie::find($id);
-        dd($movie);
         return view('movie-detail', compact('movie'));
     }
 }
